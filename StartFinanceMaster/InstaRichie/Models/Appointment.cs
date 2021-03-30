283ab4fe14d2bf1ff4a9d7540ca1c2034d631faa
Rikki Mount <rikki.mount@student.tafesa.edu.au>
2021-03-30 21:08:56 +1030

    Committing changes to feat_Appointments local branch

---
+++ StartFinanceMaster/InstaRichie/Models/Appointment.cs
@@ -0,0 +1,26 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class Appointment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AppointmentID { get; set; }
+        [NotNull]
+        public string EventName { get; set; }
+        [NotNull]
+        public string Location { get; set; }
+        [NotNull]
+        public string EventDate { get; set; }
+        [NotNull]
+        public string StartTime { get; set; }
+        [NotNull]
+        public string EndTime { get; set; }
+    }
+}
